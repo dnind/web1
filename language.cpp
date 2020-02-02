d1c385713a8779efd68b05118c4d23e15acae0fe
dnind <qudrb0417@naver.com>
2020-02-02 17:32:27 +0900

    Add files via upload

---
+++ language.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <iostream> 
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main () {
+
+	char a[100000] =
+	int size = sizeof (a);
+	bool kor = true;
+	bool space = true;
+
+	for (int i = 0; i < size; i++) {
+
+		if (a[i] & 0x80) {
+			kor = true;
+			space = true;
+		}
+		else {
+			kor = false;
+			if (space == true) {
+				printf (" ");
+			}
+			space = false;
+		}
+			;
+
+			if (kor == true) {
+				printf ("%c", a[i]);
+			}
+	}
+}
+
